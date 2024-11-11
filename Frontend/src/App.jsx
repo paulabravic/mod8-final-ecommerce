b7972cde669e637e1687cb8b59fe815cf21a240b
Adri <adrianareyesgarcia1@gmail.com>
2024-11-10 18:58:48 -0600

    Cambios App.jsx

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -2,6 +2,8 @@ import { Route, Routes } from "react-router-dom";
 import Detalle from "./views/Detalle.jsx";
 import Home from "./views/Home.jsx";
 import { Carrito } from "./views/Carrito.jsx";
+import { Login } from "./views/Login.jsx";
+import { Registro } from "./views/Registro.jsx";
 
 const App = () => {
   return (
@@ -12,6 +14,8 @@ const App = () => {
           <Route path=":id" element={<Detalle />} />
         </Route>
         <Route path="carrito" element={<Carrito />} />
+        <Route path="login" element={<Login />} />
+        <Route path="registro" element={<Registro />} />
         <Route path="/" element={<Home />} />
       </Routes>
 
