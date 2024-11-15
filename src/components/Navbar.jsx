0b27e02bb4e804342ad90bbe80dba551a6ca7aa0
paulabravic <pollybrajovic@hotmail.com>
2024-11-15 01:16:02 -0300

    Navbar icon

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -18,7 +18,7 @@ const Navbar = () => {
       <div className="container d-block">
         <div className="d-flex justify-content-between">
           <Link to="/" className="logo-nombre mx-1 mb-0">
-            <h4 className="mb-0"><img src="assets/Logos/logo_white.png" alt="Logo" width={32} /> Collares Bruno!</h4>
+            <h4 className="mb-0"><img src="/assets/Logos/logo_white.png" alt="Logo" width={32} /> Collares Bruno!</h4>
           </Link>
 
           {isLoggedIn > 0 ? (
