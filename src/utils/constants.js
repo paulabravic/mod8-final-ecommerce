e76f4495790ad8a40a00bb84477983e50a0517e2
Jose Castro <jose.castro@genera.cl>
2024-11-27 22:53:16 -0300

    Vercel

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,4 +1,5 @@
-export const URLBASE = 'http://localhost:3000'
+//export const URLBASE = 'http://localhost:3000'
+export const URLBASE = 'https://collares-bruno-backend.onrender.com'
 
 export const ENDPOINT = {
   login: `${URLBASE}/login`,
